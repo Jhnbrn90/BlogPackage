537d6517646ec3b08991ebc3b8617aa558d19bed
John Braun <john@brn.email>
2021-07-27 14:18:47 +0200

    Update install command to match documentation

--- src/Console/InstallBlogPackage.php
+++ src/Console/InstallBlogPackage.php
@@ -3,6 +3,7 @@
 namespace JohnDoe\BlogPackage\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class InstallBlogPackage extends Command
 {
@@ -15,12 +16,46 @@ class InstallBlogPackage extends Command
         $this->info('Installing BlogPackage...');
 
         $this->info('Publishing configuration...');
+        
+        if (! $this->configExists('blogpackage.php')) {
+            $this->publishConfiguration();
+            $this->info('Published configuration');
+        } else {
+            if ($this->shouldOverwriteConfig()) {
+                $this->info('Overwriting configuration file...');
+                $this->publishConfiguration($force = true);
+            } else {
+                $this->info('Existing configuration was not overwritten');
+            }
+        }
+        
+        $this->info('Installed BlogPackage');
+    }
+    
+    private function configExists($fileName)
+    {
+        return File::exists(config_path($fileName));
+    }
 
-        $this->call('vendor:publish', [
+    private function shouldOverwriteConfig()
+    {
+        return $this->confirm(
+            'Config file already exists. Do you want to overwrite it?',
+            false
+        );
+    }
+
+    private function publishConfiguration($forcePublish = false)
+    {
+        $params = [
             '--provider' => "JohnDoe\BlogPackage\BlogPackageServiceProvider",
             '--tag' => "config"
-        ]);
+        ];
 
-        $this->info('Installed BlogPackage');
+        if ($forcePublish === true) {
+            $params['--force'] = true;
+        }
+        
+        $this->call('vendor:publish', $params);
     }
 }
